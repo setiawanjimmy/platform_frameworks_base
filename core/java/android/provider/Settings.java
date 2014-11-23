8daed8587f3ac74de1706d1a461d7aee70d6546c
Roman Birg <roman@cyngn.com>
2014-11-23 06:54:06 -0800

    SystemUI: double tap status bar to sleep (1/2)
    * Make it more reliable
    * Add it to keyguard
    * Add a content observer to not always query Settings.System on every
    touch event
    
    Change-Id: I292c4d9d9f810843590b7a9ec6e15b99ac44009d
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Euphoria-OS
    * Change the activation areas for lock screen, top third and bottom near the shortcut icons
    
    Change-Id: I08bfe1fed9e3357e85b2418dda52a8f86581d93c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3579,6 +3579,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
+         /**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
