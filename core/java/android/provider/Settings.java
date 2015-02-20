9199110e1ce17674ea03f878fcb6efeaab676c25
TheMrcool212 <munirmohamadrani@gmail.com>
2015-02-20 21:42:58 +0000

    Double tap to sleep on navigation bar [1/2]
    Default off
    
    Change-Id: I5770ccd076b6b3d4e35f6fb7c80387e6dea3a4dd
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3573,6 +3573,12 @@ public final class Settings {
          */
         public static final String DISABLE_DASHBOARD_CONDITIONS = "disable_dashboard_conditions";
 
+         /**
+         *  Enable navigation bar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
