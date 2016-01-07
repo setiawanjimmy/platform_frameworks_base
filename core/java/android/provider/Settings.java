933bab6473d6aeb5fa501b26a918a7de62223753
Varun <date.varun123@gmail.com>
2016-01-07 21:52:27 +0530

    Add option to disable auto brightness icon in brightness slider
    - Off by default because this is not everyone's cup of tea
    
    - Cleaned up this commit a bit
    
    Change-Id: I9fd495f761e43e610ee1a4b91603de4c52d2b300

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4068,6 +4068,12 @@ public final class Settings {
         public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
 
         /**
+         * Whether to show Brightness Icon On Brightness Slider
+         * @hide
+         */
+        public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
