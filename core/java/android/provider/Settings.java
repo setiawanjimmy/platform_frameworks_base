a4054acf1f525bb60e3abf64325db5a4d38370e7
Alex <mazdarider23@gmail.com>
2016-09-12 17:55:33 -0400

    Disable dashboard suggestions [2/2]
    Change-Id: I33879e9b8e1ef15898e781aad4766b755df36287

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3556,6 +3556,12 @@ public final class Settings {
         public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
