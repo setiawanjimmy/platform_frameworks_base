2f6826ed84744b94f482ca6d75f6c382a679f4f8
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-21 19:58:41 +0800

    SystemUI: Implement hidden and text battery styles
    Forward port the hidden and text battery styles from CM 13.0:
    2e0a11b Forward port battery icon options
    
    Change-Id: I30d95edf9c6fe6afb343aff5effe132f94c49eeb
    
    Squashed:
    SystemUI: tuner: Remove battery preference
    https://github.com/CyanogenMod/android_frameworks_base/commit/e6c5b404014fa6876867ecb89a1762ea6cbd8ec9
    
    SystemUI: Implement battery level text customisations
    https://github.com/CyanogenMod/android_frameworks_base/commit/7d30ec36bedd785ed1526a85da9d5b64491116dc
    
    Added:
    Adjust percent text size to match clock 14sp.
    
    Add updateBatteryLevelTextView() so text scales with density or font change.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1714,7 +1714,6 @@ public class NotificationPanelView extends PanelView implements
 
     private void setListening(boolean listening) {
         mQsContainer.setListening(listening);
-        mKeyguardStatusBar.setListening(listening);
     }
 
     @Override
