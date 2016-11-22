eb18d9cdb615c3e48cd1f72ce8c2c5ec3ecd2733
joanne_chung <joanne_chung@htc.com>
2016-11-22 19:21:15 +0800

    Fix finishing Activity can receive onConfigurationChanged
    Symptom:
    finishing GrantPermissionsActivity received onConfigurationChanged
    which caused WMS throwed IllegalArgumentException
    
    Root Cause:
    WMS uses AM to updateConfiguration in sendNewConfiguration().
    AMS will call scheduleActivityConfigurationChanged to notify client
    callbacks. GrantPermissionsActivity finish Activity before received
    onConfigurationChanged. Because AMS does not check Activity
    finishing status in scheduleActivityConfigurationChanged case.
    Finishing GrantPermissionsActivity onConfigurationChanged
    called, it uses WM to removeViewImmediate().
    WMS throwes IllegalArgumentException because DecorView
    of GrantPermissionsActivity does not exist.
    
    Solution:
    Check Activity finishing state before callback
    
    Change-Id: I31dd67059948a9aa052b54f28cd6a6eff74eaec4

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -4784,7 +4784,7 @@ public final class ActivityThread {
     final void handleActivityConfigurationChanged(ActivityConfigChangeData data,
             boolean reportToActivity) {
         ActivityClientRecord r = mActivities.get(data.activityToken);
-        if (r == null || r.activity == null) {
+        if (r == null || r.activity == null || r.activity.mFinished) {
             return;
         }
 
