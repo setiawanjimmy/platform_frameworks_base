b549e133212f40529f592bcb018fea4318b4f087
ezio84 <brabus84@gmail.com>
2016-12-15 22:38:53 +0100

    Night mode tile brightness: allow to set a different value [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6242,6 +6242,12 @@ public final class Settings {
          */
         public static final String QS_NIGHT_BRIGHTNESS_TOGGLE = "qs_night_brightness_toggle";
 
+         /**
+         * Low brightness level for the advanced night mode tile
+         * @hide
+         */
+        public static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
+
         /**
          * The current night mode that has been selected by the user.  Owned
          * and controlled by UiModeManagerService.  Constants are as per
