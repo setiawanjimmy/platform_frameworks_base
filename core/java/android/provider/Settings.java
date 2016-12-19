a1ba69883c6ca40299d485454a2941d460916170
ezio84 <brabus84@gmail.com>
2016-12-19 14:38:37 +0100

    Night light tile brightness: merge the two options into one [1/2]
    smaller code for the same achievement

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6236,12 +6236,6 @@ public final class Settings {
          */
         public static final String DOZE_PULSE_ON_DOUBLE_TAP = "doze_pulse_on_double_tap";
 
-        /**
-         * Wheter to automatically change brightness when enabling night mode tile
-         * @hide
-         */
-        public static final String QS_NIGHT_BRIGHTNESS_TOGGLE = "qs_night_brightness_toggle";
-
          /**
          * Low brightness level for the advanced night mode tile
          * @hide
