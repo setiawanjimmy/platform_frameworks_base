9a6c40a29e40fa7bcf189e7b34b4a2facac33efa
ezio84 <brabus84@gmail.com>
2017-09-01 15:28:27 +0200

    Double tap to sleep on lockscreen [1/2]
    Change-Id: If8e58d7a0ebf80c19a91ffc79637b7dee21d83af

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3903,6 +3903,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
+         /**
+         * Double tap on lockscreen to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
