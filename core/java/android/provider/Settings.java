5fdf201abb1f111264e427c9315f10a80e44a2b2
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is On [1/2]
    Change-Id: Ic4e7f7d0af413497c97c83423ba7cdabf766f080

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4078,6 +4078,12 @@ public final class Settings {
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * Wheter to play notification sound and vibration if screen is on
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
